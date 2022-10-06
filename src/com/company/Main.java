ab2ca27ddcb77c4560db7c370912d74901291ed0
Moin93 <Moinmirmasomi@outlook.com>
2022-10-06 14:31:30 +0200

    just added the future that the program searches though the plantlist for the user .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,27 +5,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-
     public static void main(String[] args) {
-        växt laura=new palm(5,"Laura");
-        växt meatLoaf=new köttätande(0.7,"Meatloaf");
-        växt igge=new kaktus(0.2,"Igge");
-        växt putte=new palm(1,"Putte");
+
+        växt laura = new palm(5, "Laura");
+        växt meatLoaf = new köttätande(0.7, "Meatloaf");
+        växt igge = new kaktus(0.2, "Igge");
+        växt putte = new palm(1, "Putte");
         //Polymorfism
-        System.out.println(meatLoaf.mängd_näring()+"\n"
+        /*System.out.println(meatLoaf.mängd_näring()+"\n"
                 + laura.mängd_näring()+"\n"
                 +igge.mängd_näring()+"\n"
-                +putte.mängd_näring());
-        /*List<växt> hotellistan= Arrays.asList(laura,meatLoaf,igge,putte);
-        String input= JOptionPane.showInputDialog("vilken planta som du vill vattna?");
-        for (växt n:hotellistan) {
-            if (input.equalsIgnoreCase(n.getNamn())){
-                JOptionPane.showMessageDialog(null,n.mängd_näring());
+                +putte.mängd_näring());*/
+        List<växt> hotellistan = Arrays.asList(laura, meatLoaf, igge, putte);
+        String input = JOptionPane.showInputDialog("vilken planta som du vill vattna?");
+        for (växt n : hotellistan) {
+            if (input.equalsIgnoreCase(n.getNamn())) {
+                JOptionPane.showMessageDialog(null, n.mängd_näring());
                 break;
-            }*/
+            }else break;
         }
+    }
 }
 
 
---
+++ src/com/company/user.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class user extends växt {
+
+
+    public user(double längd, String namn) {
+        super(längd, namn);
+    }
+}
